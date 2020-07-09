3a4095fcebca1c25ec7f9c83ce383ce81f59cee6
lavish-jain <lavishrjain1997@gmail.com>
2020-07-09 23:58:09 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import PaymentDetailsForm from './components/PaymentDetailsForm/PaymentDetailsForm';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <PaymentDetailsForm />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+
+          <Switch>
+            <Route exact path='/'>
+              <PaymentDetailsForm />
+            </Route>
+            <Route path='/history'>
+              {/* <TransactionHistory /> */}
+              <h1>History</h1>
+            </Route>
+            <Route path='/refund'>
+              {/* <RefundForm /> */}
+              <h1>Refund</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <PaymentDetailsForm />
+//     </div>
+//   );
+// }
 
 export default App;
